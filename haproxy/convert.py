69e86e511ffe0ecfaebf3428ab6d2dc3dc97b949
thomaslaurenson <thomas@thomaslaurenson.com>
2025-05-27 15:11:12 +1200

    Couple old changes for user data output

--- haproxy/convert.py
+++ haproxy/convert.py
@@ -5,7 +5,7 @@ import yaml
 with open("cloud-config.yml") as f:
     data = yaml.safe_load(f)
 
-out = { "cloud-init": data }
+out = '\{"cloud-init":' + str(data) + '}'
 
 with open("cloud-config.json", "w") as f:
-    json.dump(out, f, indent=2)
+    f.write(out)
--- haproxy/vendor_data.lua
+++ haproxy/vendor_data.lua
@@ -1,20 +1,16 @@
 core.register_service("vendor_data", "http", function(applet)
-    local path = applet.path
-    if path and path:find("vendor_data") then
-        local response = vendor_data_response
-        applet:set_status(200)
-        applet:add_header("content-length", string.len(response))
-        applet:add_header("content-type", "text/plain")
-        applet:start_response()
-        applet:send(response)
-    end
+    local response = vendor_data_response
+    applet:set_status(200)
+    applet:add_header("content-length", string.len(response))
+    applet:add_header("content-type", "text/plain")
+    applet:start_response()
+    applet:send(response)
 end)
 
 core.register_init(function()
     local file_path = "/usr/local/etc/haproxy/cloud-config.json"
     local file = io.open(file_path, "r")
-    local content = file:read("*a")
+    vendor_data_response = file:read("*a")
     core.Info(content)
     file:close()
-    vendor_data_response = content
 end)
